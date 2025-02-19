8dc84216a7d74c903244c7ef58097878a488066b
Bart Smits <bart@seza.si>
2025-02-19 11:04:31 +0100

    Update startup script to include system updates and git pull before launching the server

--- startup.sh
+++ startup.sh
@@ -4,5 +4,8 @@ VENV_DIR="$APP_DIR/venv"
 SCREEN_NAME="blog"
 PYTHON_SCRIPT="main"
 
+apt update
+apt upgrade -y
 cd "$APP_DIR"
+git pull
 screen -dmS "$SCREEN_NAME" "$VENV_DIR"/bin/uvicorn "$PYTHON_SCRIPT":app --host 0.0.0.0 --port 443 --ssl-keyfile=/etc/letsencrypt/live/seza.si/privkey.pem --ssl-certfile=/etc/letsencrypt/live/seza.si/fullchain.pem
